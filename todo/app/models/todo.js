f7041ea25e134cf2dd34dfdf75785d216f3a2a5a
Kevin Woo <kevinawoo@gmail.com>
2014-04-07 16:42:51 -0700

    added validation

--- todo/app/models/todo.js
+++ todo/app/models/todo.js
@@ -1,44 +1,52 @@
 var Todo = function () {
 
-  this.defineProperties({
-    title: {type: 'string', required: true},
-    status: {type: 'string'}
-  });
+    this.defineProperties({
+        title: {type: 'string', required: true},
+        status: {type: 'string'}
+    });
 
-  /*
-  this.property('login', 'string', {required: true});
-  this.property('password', 'string', {required: true});
-  this.property('lastName', 'string');
-  this.property('firstName', 'string');
+    /*
+     this.property('login', 'string', {required: true});
+     this.property('password', 'string', {required: true});
+     this.property('lastName', 'string');
+     this.property('firstName', 'string');
 
-  this.validatesPresent('login');
-  this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
-  this.validatesLength('login', {min: 3});
-  // Use with the name of the other parameter to compare with
-  this.validatesConfirmed('password', 'confirmPassword');
-  // Use with any function that returns a Boolean
-  this.validatesWithFunction('password', function (s) {
-      return s.length > 0;
-  });
+     this.validatesPresent('login');
+     this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
+     this.validatesLength('login', {min: 3});
+     // Use with the name of the other parameter to compare with
+     this.validatesConfirmed('password', 'confirmPassword');
+     // Use with any function that returns a Boolean
+     this.validatesWithFunction('password', function (s) {
+     return s.length > 0;
+     });
 
-  // Can define methods for instances like this
-  this.someMethod = function () {
-    // Do some stuff
-  };
-  */
+     // Can define methods for instances like this
+     this.someMethod = function () gi{
+     // Do some stuff
+     };
+     */
+
+
+    this.validatesPresent('title');
+    this.validatesLength('title', {min: 5});
+
+    this.validatesWithFunction('status', function (status) {
+        return status == 'open' || status == 'done';
+    }, {message: "Status must be 'open' or 'done'."});
 
 };
 
 /*
-// Can also define them on the prototype
-Todo.prototype.someOtherMethod = function () {
-  // Do some other stuff
-};
-// Can also define static methods and properties
-Todo.someStaticMethod = function () {
-  // Do some other stuff
-};
-Todo.someStaticProperty = 'YYZ';
-*/
+ // Can also define them on the prototype
+ Todo.prototype.someOtherMethod = function () {
+ // Do some other stuff
+ };
+ // Can also define static methods and properties
+ Todo.someStaticMethod = function () {
+ // Do some other stuff
+ };
+ Todo.someStaticProperty = 'YYZ';
+ */
 
 Todo = geddy.model.register('Todo', Todo);
